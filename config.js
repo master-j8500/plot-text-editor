37b2348cd6439ed2224368c965d805fe5f94a9e1
MJ <emasterj8500@gmail.com>
2026-07-04 17:07:25 +0900

    Set OAuth client ID

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Google Cloud ConsoleでOAuthクライアントIDを発行後、ここに設定してください。
 const CONFIG = {
-  CLIENT_ID: 'YOUR_CLIENT_ID_HERE.apps.googleusercontent.com',
+  CLIENT_ID: '370386075095-p1prv747f20uvgj91fkrrbu7nuam868d.apps.googleusercontent.com',
   FOLDER_ID: '1tKanYbcc7g1dbMaDuajZlehx9QdyBkur', // Plot TEXT フォルダ
   SCOPES: 'https://www.googleapis.com/auth/drive',
 };
